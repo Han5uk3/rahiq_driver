54f57ed166d138757689009edc94baff88c7e828
Han5uk3 <adnanyousufpangat@gmail.com>
2026-08-01 21:11:49 +0530

    fix: adjust bottom navigation padding for improved layout

--- lib/pages/shared/custom_bottom_nav.dart
+++ lib/pages/shared/custom_bottom_nav.dart
@@ -24,7 +24,7 @@ class CustomBottomNavBar extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       color: Colors.transparent,
-      padding: const EdgeInsetsDirectional.fromSTEB(16, 16, 16, 36),
+      padding: const EdgeInsetsDirectional.fromSTEB(16, 16, 16, 16),
       child: Container(
         height: 85,
         decoration: BoxDecoration(
